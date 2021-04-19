91c2aa8ade61711cca36cda6cf5fdc1ce1d5e4b2
Michael Pingel <mdpingel@gmail.com>
2021-04-19 15:46:48 -0700

    add ProfileAbout.js
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

---
+++ src/components/ProfileAbout.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+function ProfileAbout() {
+  return (
+    <React.Fragment>
+      <p>
+        Once a smooth-talking smuggler, Rando Calrissian changed from a get-rich-quick schemer to a selfless leader in the fight against the Empire. From adventures with the Ghost crew of rebels to the attack on Death Star II, his quick wit and daring proved to be invaluable.
+      </p>
+    </React.Fragment>
+  );
+}
+
+// ProfileAbout.propTypes = {
+//   about: PropTypes.string.isRequired
+// }
+
+export default ProfileAbout;
