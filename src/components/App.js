d860056ee36739aec00ce6f4cd1558366ab2989e
Michael Pingel <mdpingel@gmail.com>
2021-04-19 15:47:38 -0700

    update app.js
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './Header';
 import Profile from './Profile';
 import Feed from './Feed';
 import Suggested from './Suggested';
@@ -6,9 +7,20 @@ import Suggested from './Suggested';
 function App(){
   return (
     <React.Fragment>
-      <Profile />
-      <Feed />
-      <Suggested />
+      <div class="container">
+        <Header />
+        <div class="row">
+          <div class="col-4">
+            <Profile />
+          </div>
+          <div class="col-4">
+            <Feed />
+          </div>
+          <div class="col-4">
+            <Suggested />
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   )
 }
