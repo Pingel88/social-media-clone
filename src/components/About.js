9e22f19b9e0ca4f8e4772c6e53a38275c5cf3202
Michael Pingel <mdpingel@gmail.com>
2021-04-19 14:12:10 -0700

    create About.js
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h3>
+        Once a smooth-talking smuggler, Lando Calrissian changed from a get-rich-quick schemer to a selfless leader in the fight against the Empire. From adventures with the Ghost crew of rebels to the attack on Death Star II, his quick wit and daring proved to be invaluable.
+      </h3>
+    </React.Fragment>
+  );
+}
+
+// About.propTypes = {
+//   about: PropTypes.string.isRequired
+// }
+
+export default About;
