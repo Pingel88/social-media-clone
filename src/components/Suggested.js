b8ed303caa609b2a344eac76c3b64502823a6fb0
Michael Pingel <mdpingel@gmail.com>
2021-04-19 15:50:32 -0700

    update Suggested.js
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- src/components/Suggested.js
+++ src/components/Suggested.js
@@ -1,10 +1,13 @@
 import React from "react";
 import SuggestedList from "./SuggestedList";
 
-function Profile() {
+function Suggested() {
   return (
     <React.Fragment>
+      <h3>Suggested Friends</h3>
       <SuggestedList />
     </React.Fragment>
   );
 }
+
+export default Suggested;
