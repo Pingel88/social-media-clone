3931fa8e3bf5a19341fe7cc1f1ca9370d3504520
Michael Pingel <mdpingel@gmail.com>
2021-04-19 15:46:15 -0700

    add HeaderNav.js
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

---
+++ src/components/HeaderNav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HeaderNav() {
+  return (
+    <React.Fragment>
+      <button class="btn btn-outline-primary">Home</button>
+      <button class="btn btn-outline-primary">Notifications</button>
+      <button class="btn btn-outline-primary">Messages</button>
+    </React.Fragment>
+  )
+}
+
+export default HeaderNav;
