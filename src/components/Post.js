5299d2c5f14f119f24614c074abf91d7e060b243
Michael Pingel <mdpingel@gmail.com>
2021-04-19 15:48:23 -0700

    update Post.js
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Post(props){
   return (
     <React.Fragment>
+      <img src={props.pic} alt="profile pic" class="feed-pic"/>
       <h4>{props.name}</h4>
       <p>{props.update}</p>
       <hr/>
