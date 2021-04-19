355d8258203661cb0f0be73b3aff07377cd5eed0
Michael Pingel <mdpingel@gmail.com>
2021-04-19 14:13:49 -0700

    create Suggested.js
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

---
+++ src/components/Suggested.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SuggestedList from "./SuggestedList";
+
+function Profile() {
+  return (
+    <React.Fragment>
+      <SuggestedList />
+    </React.Fragment>
+  );
+}
