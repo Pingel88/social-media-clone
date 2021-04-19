1d43c5b225da189d8c77839473ccc4dff53cd1dd
Michael Pingel <mdpingel@gmail.com>
2021-04-19 14:13:22 -0700

    create Profile.js
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Name from "./Name";
+import About from "./About";
+
+function Profile() {
+  return (
+    <React.Fragment>
+      <Name />
+      <About />
+    </React.Fragment>
+  );
+}
+
+export default Profile;
