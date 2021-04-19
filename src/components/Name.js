8d7a2cb87c928a5c16f9829fbef7bd4374830961
Michael Pingel <mdpingel@gmail.com>
2021-04-19 14:12:52 -0700

    create Name.js
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

---
+++ src/components/Name.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+function Name() {
+  return (
+  <React.Fragment>
+    <h3>Rando Calrissian</h3>
+  </React.Fragment>
+  );
+}
+
+// Name.propTypes = {
+//   name: PropTypes.string.isRequired
+// }
+
+export default Name;
