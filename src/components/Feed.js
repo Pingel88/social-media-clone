7f1883bf9fb9c1ddf971b38c3eb70c7373d3e7a5
Michael Pingel <mdpingel@gmail.com>
2021-04-19 15:47:52 -0700

    update Feed.js
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,12 +1,16 @@
 import React from "react";
-import NewPost from "./NewPost";
 import PostList from "./PostList";
+import NewPost from "./NewPost";
 
 function Feed() {
   return (
     <React.Fragment>
+      <h3>News Feed</h3>
       <NewPost />
+      <hr/>
       <PostList />
     </React.Fragment>
   );
 }
+
+export default Feed;
